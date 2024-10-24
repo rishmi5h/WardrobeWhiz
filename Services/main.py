003f9b5f729107815a0844fa885cfb70f382ac37
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-10-25 05:19:07 +0530

    add : postgres connection

--- Services/main.py
+++ Services/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
 app = FastAPI()
 
+# Database connection setup
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+        
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
