37d1dd10fd33357796c056a0515cc69b90a1ba7f
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-10-29 02:07:39 +0530

    update : main app

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -1,10 +1,22 @@
-import App from './App.tsx';
-import './App.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import GlobalStyle from './styles/global';
+import { theme } from './styles/theme';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
